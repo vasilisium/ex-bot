987b2146dba7f81ae5d1951a7d6890013a46a984
vasilisium <vasilisium@gmail.com>
2021-02-06 21:27:22 +0200

    added some files for Heroku hosting

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 
-BOT_API_KEY=os.getenv('BOT_KEY')
+BOT_API_KEY=os.environ['BOT_KEY']
 
 logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s' \
   , level=logging.DEBUG, filename='log.txt')
